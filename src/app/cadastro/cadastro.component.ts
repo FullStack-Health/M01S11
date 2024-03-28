f3929d265b31b2e696a1ca00f947f308f226cc5e
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-28 18:04:04 -0300

    Atividade Lista de Produtos - parte 13

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ReactiveFormsModule], //Importação do Modulo de Reative Forms
+  imports: [ReactiveFormsModule, CommonModule], //Importação do Modulo de Reative Forms
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
   form = new FormGroup({
-    nome: new FormControl('', [Validators.minLength(3), Validators.required]),
-    codigoUsuario: new FormControl('', [Validators.minLength(5),Validators.maxLength(10), Validators.required]),
+    nome: new FormControl('', Validators.required),
+    codigoUsuario: new FormControl('', [Validators.minLength(5), Validators.maxLength(10), Validators.required]),
     senha: new FormControl('', [Validators.minLength(4), Validators.required]),
     confirmarSenha: new FormControl('', [Validators.minLength(4), Validators.required]),
   });
