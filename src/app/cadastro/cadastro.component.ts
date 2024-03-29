4944fdf69375e5fc22214194d68acd5fe0a19c9e
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-03-28 22:05:36 -0300

    Atividade Lista de Produtos - parte 15 e 16

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomValidatorService } from '../shared/services/custom-validator.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule], //Importação do Modulo de Reative Forms
-  providers: [CustomValidatorService],
+  imports: [ReactiveFormsModule, CommonModule, MatButtonModule, MatIconModule, ToastModule], //Importação do Modulo de Reative Forms
+  providers: [CustomValidatorService, MessageService],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
@@ -19,15 +23,20 @@ export class CadastroComponent {
     confirmarSenha: new FormControl('', [Validators.minLength(4), Validators.required]),
   });
 
-  constructor(private customValidatorService: CustomValidatorService) {}
+  constructor(
+    private customValidatorService: CustomValidatorService,
+    private messageService: MessageService) {}
 
   cadastrar() {
-    if (this.form.value.senha === this.form.value.confirmarSenha) {
+    if (this.form.valid) {
       const usuario = {
         codigoUsuario: this.form.value.codigoUsuario,
         senha: this.form.value.senha
       }
       localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+      this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Usuário logado' });
+    } else {
+      this.messageService.add({ severity: 'error', summary: 'Falha', detail: 'Formulário inválido. Verifique o preenchimento para continuar' });
     }
   }
 }
